9d3d2f5cfe1ec62d4b4a68f826ab2010b1993f75
hjulstromandre <152264441+hjulstromandre@users.noreply.github.com>
2024-08-28 20:08:40 +0200

    webcam integration

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from tensorflow.keras.models import load_model
 import os
 from dotenv import load_dotenv
 import gdown
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 
 load_dotenv()
 
@@ -35,29 +36,23 @@ def preprocess_face(face):
     roi = np.expand_dims(roi, axis=0)
     return roi
 
-def main():
-    cap = cv2.VideoCapture(0)
-    stframe = st.empty()
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            st.write("Failed to capture video")
-            break
-        frame = cv2.flip(frame, 1)
-        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+class EmotionDetector(VideoTransformerBase):
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+        gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
         for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-            face = frame[y:y+h, x:x+w]
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            face = img[y:y+h, x:x+w]
             preprocessed_face = preprocess_face(face)
             prediction = model.predict(preprocessed_face)[0]
             predicted_class_index = np.argmax(prediction)
             predicted_label = emotion_labels[predicted_class_index]
             confidence_score = prediction[predicted_class_index]
             text = f'{predicted_label} ({confidence_score*100:.2f}%)'
-            cv2.putText(frame, text, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-        stframe.image(frame, channels="BGR")
-    cap.release()
+            cv2.putText(img, text, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
-if st.button('Start Webcam'):
-    main()
+        return img
+
+webrtc_streamer(key="emotion-detector", video_transformer_factory=EmotionDetector)
